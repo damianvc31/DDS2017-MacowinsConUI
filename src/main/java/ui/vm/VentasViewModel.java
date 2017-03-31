57ad7e170b76d08ee0219edb31a333be4064842b
damianvc31 <damiancohen93@gmail.com>
2017-03-31 13:15:28 -0300

    Agrego solucion alternativa (asignando ventas a null)

--- src/main/java/ui/vm/VentasViewModel.java
+++ src/main/java/ui/vm/VentasViewModel.java
@@ -2,12 +2,15 @@ package ui.vm;
 
 import java.util.List;
 
-import org.uqbar.commons.utils.TransactionalAndObservable;
+import org.uqbar.commons.utils.Observable;
+
+//import org.uqbar.commons.utils.TransactionalAndObservable;
 
 import model.Venta;
 import model.repositories.Repositorios;
 
-@TransactionalAndObservable
+//@TransactionalAndObservable //alternativa para solucionar problema de actualizacion de tabla
+@Observable
 public class VentasViewModel {
 
 	private List<Venta> ventas;
@@ -26,7 +29,8 @@ public class VentasViewModel {
 
 	public void registrarVenta(Venta venta) {
 		Repositorios.ventas.agregar(venta);
-		this.setVentas(Repositorios.ventas.all());
+		this.setVentas(null); //se reasigna la lista de ventas a la misma para que lo actualice
+		this.setVentas(Repositorios.ventas.all()); // la vista
 	}
 	
 }
